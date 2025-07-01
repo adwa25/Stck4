b2d4017c18199773480d31636b71329c932324d9
adwa25 <Zzilongcc@gmail.com>
2025-07-01 14:07:58 +0700

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,1 @@
 
-// Pendekatan modular - Lihat pesan sebelumnya untuk penjelasan
-// Script ini termasuk fitur musuh, darah, suara, senjata, toko, dan NPC
-// Fokus: Modular & tidak ubah kode awal user (semua di luar kode utama ditambahkan)
-
-// (Kode JS lengkap akan disiapkan setelah ini karena panjang)
